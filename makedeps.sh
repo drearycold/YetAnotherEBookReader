86e31e73a04dc72b1e7678ec906466a651a37305
Peter <peterli@163.com>
2021-09-18 11:51:01 +0800

    tweaking depencencies

--- makedeps.sh
+++ makedeps.sh
@@ -1,6 +1,5 @@
-carthage update --no-build --use-submodules
-for DEP in `cat Cartfile ./Carthage/Checkouts/*/Cartfile | fgrep github | fgrep -v FolioReaderKit| awk '{print $2}' | sed -e 's/"//g' | awk -F'/' '{print $2}'`
+carthage update --no-build
+for DEP in `cat Cartfile ./Carthage/Checkouts/*/Cartfile | fgrep github | awk '{print $2}' | sed -e 's/"//g' | awk -F'/' '{print $2}'`
 do
-	carthage build --platform ios,macos --use-xcframeworks --cache-builds --no-use-binaries $DEP
-	#carthage build --platform ios --use-xcframeworks $DEP
+	carthage build --platform ios --use-xcframeworks --cache-builds --no-use-binaries $DEP
 done
