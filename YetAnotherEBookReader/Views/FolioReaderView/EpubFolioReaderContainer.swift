8eeb1bc695cbc6e187e36d98a6c1bf5de02a8347
Drearycold <roswen9@gmail.com>
2022-05-17 10:36:08 +0800

    WIP: more vertical writing mode positioning fix, extract web server from folio reader lib to outer app

--- YetAnotherEBookReader/Views/FolioReaderView/EpubFolioReaderContainer.swift
+++ YetAnotherEBookReader/Views/FolioReaderView/EpubFolioReaderContainer.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 import FolioReaderKit
+import GCDWebServer
+import ZIPFoundation
+
+internal let kGCDWebServerPreferredPort = 46436
 
 @available(macCatalyst 14.0, *)
 class EpubFolioReaderContainer: FolioReaderContainer, FolioReaderDelegate {
@@ -18,6 +22,10 @@ class EpubFolioReaderContainer: FolioReaderContainer, FolioReaderDelegate {
     
     var folioReaderPreferenceProvider: FolioReaderPreferenceProvider?
     var folioReaderHighlightProvider: FolioReaderHighlightProvider?
+
+    let webServer = GCDWebServer()
+    let dateFormatter = DateFormatter()
+    var epubArchive: Archive?
 
     func open(bookReadingPosition: BookDeviceReadingPosition) {
         readerConfig.loadSavedPositionForCurrentBook = true
@@ -45,10 +53,15 @@ class EpubFolioReaderContainer: FolioReaderContainer, FolioReaderDelegate {
     func folioReader(_ folioReader: FolioReader, didFinishedLoading book: FRBook) {
         folioReader.readerCenter?.delegate = MyFolioReaderCenterDelegate()
         folioReader.readerCenter?.pageDelegate = yabrFolioReaderPageDelegate
+        
+        self.epubArchive = book.epubArchive
+        initializeWebServer()
+        readerConfig.serverPort = Int(webServer.port)
     }
     
     func folioReaderDidClose(_ folioReader: FolioReader) {
         updateReadingPosition(folioReader)
+        webServer.stop()
     }
     
     func updateReadingPosition(_ folioReader: FolioReader) {
@@ -93,5 +106,120 @@ class EpubFolioReaderContainer: FolioReaderContainer, FolioReaderDelegate {
         modelData?.updatedReadingPosition = updatedReadingPosition
     }
 
+    open func initializeWebServer() -> Void {
+        dateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss zzz"
+        dateFormatter.locale = Locale(identifier: "en_US")
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        webServer.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self) { request in
+            guard let path = request.path.removingPercentEncoding else { return GCDWebServerErrorResponse() }
+            print("\(#function) GCDREQUEST path=\(path)")
+            
+            var pathSegs = path.split(separator: "/")
+            guard pathSegs.count > 1 else { return GCDWebServerErrorResponse() }
+            pathSegs.removeFirst()
+            let resourcePath = pathSegs.joined(separator: "/")
+            
+            guard let archive = self.epubArchive,
+                  let entry = archive[resourcePath] else { return GCDWebServerErrorResponse() }
+            
+            var contentType = GCDWebServerGetMimeTypeForExtension((resourcePath as NSString).pathExtension, nil)
+            if contentType.contains("text/") {
+                contentType += ";charset=utf-8"
+            }
+            
+            var dataQueue = [Data]()
+            var isError = false
+            
+            let streamResponse = GCDWebServerStreamedResponse(
+                contentType: contentType,
+                asyncStreamBlock: { block in
+                    DispatchQueue.global(qos: .userInteractive).async {
+                        while( dataQueue.isEmpty && isError == false ) {
+                            Thread.sleep(forTimeInterval: 0.001)
+                        }
+                        print("\(#function) async-stream-block \(resourcePath) dataQueueCount=\(dataQueue.count)")
+                        
+                        DispatchQueue.main.async {
+                            if isError {
+                                block(nil, UncompressError())
+                            } else {
+                                block(dataQueue.removeFirst(), nil)
+                            }
+                        }
+                    }
+                }
+            )
+            
+            if let modificationDate = entry.fileAttributes[.modificationDate] as? Date {
+                streamResponse.setValue(self.dateFormatter.string(from: modificationDate), forAdditionalHeader: "Last-Modified")
+                streamResponse.cacheControlMaxAge = 86400
+            }
+            
+            var totalCount = 0
+            let entrySize = entry.uncompressedSize
+            DispatchQueue.global(qos: .userInitiated).async {
+                do {
+                    let _ = try archive.extract(entry) { data in
+                        while( dataQueue.count > 4) {
+                            Thread.sleep(forTimeInterval: 0.001)
+                        }
+                        let d = Data(data)
+                        DispatchQueue.main.async {
+                            dataQueue.append(d)
+                            totalCount += data.count
+                            if totalCount >= entrySize {
+                                dataQueue.append(Data())
+                            }
+                        }
+                        print("\(#function) zipfile-deflate \(resourcePath) dataCount=\(data.count)")
+                    }
+                } catch {
+                    isError = true
+                }
+            }
+            
+            return streamResponse
+        }
+        
+        webServer.addHandler(forMethod: "GET", pathRegex: "^/_fonts/.+?(otf|ttf)$", request: GCDWebServerRequest.self) { request in
+            let fileName = (request.path as NSString).lastPathComponent
+            print("\(#function) GCDREQUEST FONT fileName=\(fileName) path=\(request.path)")
+
+            guard let documentDirectory = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            else { return nil }
+            
+            let fontFileURL = documentDirectory.appendingPathComponent("Fonts",  isDirectory: true).appendingPathComponent(fileName, isDirectory: false)
+            guard FileManager.default.fileExists(atPath: fontFileURL.path) else { return GCDWebServerErrorResponse() }
+            
+            guard let fileResponse = GCDWebServerFileResponse(file: fontFileURL.path) else { return GCDWebServerErrorResponse() }
+            
+            return fileResponse
+        }
+        
+        try? webServer.start(options: [
+            GCDWebServerOption_Port: kGCDWebServerPreferredPort,
+            GCDWebServerOption_BindToLocalhost: true,
+            GCDWebServerOption_AutomaticallySuspendInBackground: false
+        ])
+        
+        // fallback
+        if webServer.isRunning == false {
+            try? webServer.start(options: [
+                GCDWebServerOption_BindToLocalhost: true,
+                GCDWebServerOption_AutomaticallySuspendInBackground: false
+            ])
+            
+            if webServer.isRunning == false {
+                try? webServer.start(options: [
+                    GCDWebServerOption_BindToLocalhost: true
+                ])
+            }
+        }
+        
+    }
 }
 
+struct UncompressError: Error {
+    
+}
